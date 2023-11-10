45ec3fb5598f8c7bc0310eb4eadc9dca4770211a
Jatsiri Martinez <mar18077@byui.edu>
2023-11-09 19:55:25 -0600

    W02: Programming Task

---
+++ scripts/w02-task.js
@@ -0,0 +1,33 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName       = "Jatsiri Detzaní Martínez Hernández";
+let currentYear    = 2023;
+let profilePicture = 'images/me.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement  = document.getElementById('name');
+const foodElement  = document.getElementById('food');
+const yearElement  = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML   = `<strong> ${fullName} </strong>`;
+yearElement.textContent = currentYear;
+imageElement.src        = profilePicture;
+imageElement.alt        = `profile image of ${fullName}`;
+
+/* Step 5 - Array */
+const favFood         = ["Enchiladas", "Tacos", "Pozole", "Tostadas"];
+foodElement.innerHTML = ` ${favFood} `;
+let anotherFood       = "Boneless";
+
+favFood.push(anotherFood);
+foodElement.innerHTML += `<br> ${favFood}`;
+favFood.shift();
+foodElement.innerHTML += `<br> ${favFood}`;
+favFood.pop();
+foodElement.innerHTML += `<br> ${favFood}`;
